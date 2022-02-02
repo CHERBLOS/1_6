124a2f291bf5f21cf3d9bb87ed44daeddfddfb78
CHERBLOS <92993083+CHERBLOS@users.noreply.github.com>
2022-02-02 20:22:21 +0300

    Update sidebar.js

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -3,7 +3,6 @@ let burgerButtonClose = sidebarActivate.querySelector(".sidebar__menu");
 let overlayClose = sidebarActivate.querySelector(".overlay");
 let burgerButtonOpen = document.querySelector(".header__menu-button");
 
-console.log(sidebarActivate);
 burgerButtonOpen.addEventListener("click", function sidebarOpener() {
   sidebarActivate.classList.add("sidebar--active");
 });
